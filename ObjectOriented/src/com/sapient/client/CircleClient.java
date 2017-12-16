279be6206d524aa1caa6f29eac24f235ae69cd85
v_ggovin <v_ggovin@GIVSARDIN46607.sapient.com>
2017-12-16 18:32:54 +0530

    govind modified at line no 12

--- ObjectOriented/src/com/sapient/client/CircleClient.java
+++ ObjectOriented/src/com/sapient/client/CircleClient.java
@@ -9,7 +9,7 @@ public class CircleClient {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the amount");
+		System.out.println("Enter the principle");
 		double rad = scan.nextDouble();
 		Circle obj = new Circle();
 		obj.radius=rad;
