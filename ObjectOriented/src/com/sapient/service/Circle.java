5a0afed65ab3ad47b51ad8c88c20ca8e7e3ec910
vvali2 <vvali2@GIVSARDIN38996.sapient.com>
2017-12-16 17:13:47 +0530

    varun added comment to class circle

--- ObjectOriented/src/com/sapient/service/Circle.java
+++ ObjectOriented/src/com/sapient/service/Circle.java
@@ -1,14 +1,23 @@
 package com.sapient.service;
 
+/*
+ * Ciorcle class have 3 states and 2 bhaviour
+ */
 public class Circle {
 
 	public double radius;
 	
+	/* 
+	 * performs area of a circle
+	 */
 	public double calcArea(){
 		double area = Math.PI * radius * radius;
 		return area;
 	}
 	
+	/* 
+	 * performs area of a perimeter
+	 */
 	public double calcPeri(){
 		double pmtr = 2 * Math.PI * radius ;
 		return pmtr;
